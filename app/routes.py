38adb58ef04749232ebc13d2d591257a19c9fd47
Anshuman Singh <jaspreetsinghaniya64@gmail.com>
2025-03-14 20:04:32 +0530

    Internal Process

--- app/routes.py
+++ app/routes.py
@@ -34,15 +34,15 @@ def register():
         username = request.form["username"]
         email = request.form["email"]
         password = bcrypt.generate_password_hash(request.form["password"]).decode('utf-8')
-        role=request.form["role"]
-        user = User(username=username, email=email, password=password,role=role)
+        
+        user = User(username=username, email=email, password=password)
         db.session.add(user)
         db.session.commit()
         flash("Registration successful!", "success")
         return redirect(url_for('main.login'))
     return render_template("auth/register.html")
 
-@main.route("/login", method=["GET", "POST"])
+@main.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form["email"]
