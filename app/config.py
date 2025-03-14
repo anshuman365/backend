e3c83a46e959c56fd9a913d56e195b1d69473f1c
Anshuman Singh <jaspreetsinghaniya64@gmail.com>
2025-03-14 19:09:53 +0530

    Update in requirments.txt

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = "postgresql://e_commerce_database_83mb_user:w3ApiBl9lcavvc17VpmAh7ykgIdwod0a@dpg-cva14gpc1ekc738orhn0-a:5432/e_commerce_database_83mb"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_TIMEOUT = 30
     MAIL_SERVER = 'smtp.gmail.com'
